c7cfb58e10ab48d9efb9a2e49ad0d525da83d865
Josiah Nunemaker <josiah.nunemaker@gmail.com>
2017-10-03 15:18:27 -0400

    Add Vue routing for seperate pages

--- js/app.js
+++ js/app.js
@@ -1,17 +1,31 @@
 import '../styles/mobile.scss';
 import * as example from './example.js';
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 
+// Import the pages
+import home from './home';
+import about from './about';
+
+Vue.config.devtools = true;
+
+Vue.use(VueRouter);
+
+const routes = [
+  {path: '/about', component: about},
+  {path: '/', component: home},
+];
+
+const router = new VueRouter({
+  routes,
+});
 example.sayHello();
 
 console.log('Eureka! It worked!');
 
-let app = new Vue({
-  el: '#content',
-  data: {
-    pageContent: 'Hey there!',
-  },
-});
+const app = new Vue({
+  router,
+}).$mount('#content');
 
 module.exports = {
   app: app,
